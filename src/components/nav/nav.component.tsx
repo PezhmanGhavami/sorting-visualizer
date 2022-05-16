e306ba60859e820819921f17f66afce288f76eda
Pezhman Ghavami <pejmanghavami@gmail.com>
2022-05-16 07:43:08 +0000

    Made the visual apporoximation of what I have in mind for nav, it doesn't have functionality yet

--- src/components/nav/nav.component.tsx
+++ src/components/nav/nav.component.tsx
@@ -25,33 +25,48 @@ interface INavProps {
 }
 
 enum SortTypes {
-  bubble = "bubble",
-  insertion = "insertion",
-  selection = "selection",
-  merge = "merge",
-  quick = "quick",
+  BUBBLE = "bubble",
+  INSERTION = "insertion",
+  SELECTION = "selection",
+  MERGE = "merge",
+  QUICK = "quick",
+}
+
+enum InputChangeTypes {
+  BAR_COUNT = "barCount",
+  ANIMATION_SPEED = "animationSpeed",
+  SORT_TIMELINE = "sortTimeline",
 }
 
 type SortTypesKey = keyof typeof SortTypes;
 
 const Nav: FC<INavProps> = (props) => {
   const [sortType, setSortType] = useState(
-    SortTypes.bubble
+    SortTypes.BUBBLE
   );
+
+  const [temp, setTemp] = useState(1);
 
   const handleInputChange: ChangeEventHandler<
     HTMLInputElement
   > = (event) => {
-    if (event.currentTarget.name === "barCount") {
-      props.changeBarCount(
-        parseInt(event.currentTarget.value) || 2
-      );
-    } else if (
-      event.currentTarget.name === "animationSpeed"
-    ) {
-      props.changeAnimationSpeed(
-        parseInt(event.currentTarget.value) || 1
-      );
+    const changeAction = event.currentTarget.name;
+    switch (changeAction) {
+      case InputChangeTypes.BAR_COUNT: {
+        props.changeBarCount(
+          parseInt(event.currentTarget.value) || 2
+        );
+        break;
+      }
+      case InputChangeTypes.ANIMATION_SPEED: {
+        props.changeAnimationSpeed(
+          parseInt(event.currentTarget.value) || 1
+        );
+        break;
+      }
+      case InputChangeTypes.SORT_TIMELINE: {
+        setTemp(parseInt(event.currentTarget.value) || 1);
+      }
     }
   };
 
@@ -69,19 +84,19 @@ const Nav: FC<INavProps> = (props) => {
   ) => {
     event.preventDefault();
     switch (sortType) {
-      case SortTypes.bubble:
+      case SortTypes.BUBBLE:
         props.bubbleSort();
         break;
-      case SortTypes.insertion:
+      case SortTypes.INSERTION:
         props.insertionSort();
         break;
-      case SortTypes.selection:
+      case SortTypes.SELECTION:
         props.selectionSort();
         break;
-      case SortTypes.merge:
+      case SortTypes.MERGE:
         props.mergeSort();
         break;
-      case SortTypes.quick:
+      case SortTypes.QUICK:
         props.quickSort();
         break;
     }
@@ -89,74 +104,89 @@ const Nav: FC<INavProps> = (props) => {
   return (
     <div className="nav">
       <form className="nav__form" onSubmit={handleSubmit}>
-        <button
-          className="nav__form__item"
-          type="button"
-          onClick={props.resetTheArray}
-        >
-          Generate New Array
-        </button>
-
-        <div className="nav__form__item">
-          <label htmlFor="bar-count">Bar Count: </label>
-          <input
-            type="range"
-            name="barCount"
-            id="bar-count"
-            min={2}
-            max={props.barInfo.maxBarsForWidth}
-            step={1}
-            value={props.barInfo.barCount}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div className="nav__form__item">
-          <label htmlFor="animation-speed">
-            Frame Time:{" "}
-          </label>
-          <input
-            type="number"
-            name="animationSpeed"
-            id="animation-speed"
-            min={1}
-            max={500}
-            step={1}
-            value={props.animationSpeed}
-            onChange={handleInputChange}
-          />
-        </div>
-
-        <div className="nav__form__item">
-          <label htmlFor="sort-type">
-            Sort Algorithm:{" "}
-          </label>
-          <select
-            id="sort-type"
-            name="sortType"
-            value={sortType}
-            onChange={handleSelectChange}
+        <div className="nav-settings">
+          <button
+            className="nav__form__item"
+            type="button"
+            onClick={props.resetTheArray}
           >
-            <option value={SortTypes.bubble}>
-              Bubble Sort
-            </option>
-            <option value={SortTypes.insertion}>
-              Insertion Sort
-            </option>
-            <option value={SortTypes.selection}>
-              Selection Sort
-            </option>
-            <option value={SortTypes.merge}>
-              Merge Sort
-            </option>
-            <option value={SortTypes.quick}>
-              Quick Sort
-            </option>
-          </select>
+            Generate New Array
+          </button>
+          <div className="nav__form__item">
+            <label htmlFor="bar-count">Bar Count: </label>
+            <input
+              type="range"
+              name={InputChangeTypes.BAR_COUNT}
+              id="bar-count"
+              min={2}
+              max={props.barInfo.maxBarsForWidth}
+              step={1}
+              value={props.barInfo.barCount}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="nav__form__item">
+            <label htmlFor="animation-speed">
+              Frame Time:{" "}
+            </label>
+            <input
+              type="number"
+              name={InputChangeTypes.ANIMATION_SPEED}
+              id="animation-speed"
+              min={1}
+              max={500}
+              step={1}
+              value={props.animationSpeed}
+              onChange={handleInputChange}
+            />
+          </div>
+
+          <div className="nav__form__item">
+            <label htmlFor="sort-type">
+              Sort Algorithm:{" "}
+            </label>
+            <select
+              id="sort-type"
+              name="sortType"
+              value={sortType}
+              onChange={handleSelectChange}
+            >
+              <option value={SortTypes.BUBBLE}>
+                Bubble Sort
+              </option>
+              <option value={SortTypes.INSERTION}>
+                Insertion Sort
+              </option>
+              <option value={SortTypes.SELECTION}>
+                Selection Sort
+              </option>
+              <option value={SortTypes.MERGE}>
+                Merge Sort
+              </option>
+              <option value={SortTypes.QUICK}>
+                Quick Sort
+              </option>
+            </select>
+          </div>
         </div>
-        <button className="nav__form__item" type="submit">
-          Sort
-        </button>
+
+        <div className="nav-controllers">
+          <input
+            // className="nav__form__item"
+            type="range"
+            name={InputChangeTypes.SORT_TIMELINE}
+            id="sort-timeline"
+            value={temp}
+            min={1}
+            max={100}
+            onChange={handleInputChange}
+            disabled
+          />
+          <button className="nav__form__item" type="submit">
+            Sort
+          </button>
+        </div>
       </form>
     </div>
   );
